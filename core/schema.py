6ac225a269f344b2dd85c62724ddd0791827f9d2
Arnav Dixit <arnavdixit127@gmail.com>
2022-02-11 23:11:36 +0530

    feat: add patientrecord queries

--- core/schema.py
+++ core/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from user.schema import AuthMutation, AuthQuery, HospitalQuery, PatientQuery, PatientMutation, PatientAuthorizedHospitalQuery, PatientAuthorizedHospitalMutation
-
+from patientrecord.schema import MedicineRecordQuery, DoctorNotesQuery, TestResultQuery
 
 from resources.schema import HospitalResourceQuery
 class Query(
@@ -8,6 +8,9 @@ class Query(
     PatientQuery,
     PatientAuthorizedHospitalQuery,
     HospitalQuery,
+    MedicineRecordQuery,
+    DoctorNotesQuery,
+    TestResultQuery,
     AuthQuery, 
     graphene.ObjectType,
     ):
