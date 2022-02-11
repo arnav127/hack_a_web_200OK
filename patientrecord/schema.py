b75ed1ba16d337b6c7e9aabff38fa2f44320fce2
Arnav Dixit <arnavdixit127@gmail.com>
2022-02-12 02:19:19 +0530

    feat: create `test_result` mutation

--- patientrecord/schema.py
+++ patientrecord/schema.py
@@ -183,3 +183,41 @@ class DoctorNotesMutation(graphene.ObjectType):
     create_doctor_notes = CreateDoctorNotes.Field()
     update_doctor_notes = UpdateDoctorNotes.Field()
 
+
+class CreateTestResult(graphene.Mutation):
+    class Arguments:
+        patient = graphene.String()
+        test_name = graphene.String()
+        test_result = graphene.String()
+
+    test_result = graphene.Field(TestResultType)
+
+    @classmethod
+    @login_required
+    def mutate(self, root, info, patient, test_name, test_result):
+        test_result = TestResult.objects.create(patient=patient, test_name=test_name, test_result=test_result)
+        return CreateTestResult(test_result=test_result)
+
+
+class UpdateTestResult(graphene.Mutation):
+    class Arguments:
+        id = graphene.String(required=True)
+        patient = graphene.String()
+        test_name = graphene.String()
+        test_result = graphene.String()
+
+    test_result = graphene.Field(TestResultType)
+
+    @classmethod
+    @login_required
+    def mutate(self, root, info, id, **kwargs):
+        test_result = TestResult.objects.get(pk=id)
+        for k, v in kwargs.items():
+            setattr(test_result, k, v)
+        test_result.save()
+        return UpdateTestResult(test_result=test_result)
+
+
+class TestResultMutation(graphene.ObjectType):
+    create_test_result = CreateTestResult.Field()
+    update_test_result = UpdateTestResult.Field()
