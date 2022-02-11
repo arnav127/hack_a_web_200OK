94cc0b6d367ff930c74af2f1ebc0fb3c3a145a49
Gyanendra Shukla <srirajshukla@gmail.com>
2022-02-11 20:52:35 +0530

    feat: add doctor notes and test result record type

--- patientrecord/admin.py
+++ patientrecord/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from patientrecord.models import MedicinePrescription, MedicineRecord
+from patientrecord.models import DoctorNotes, MedicinePrescription, MedicineRecord, TestResult
 
 # Register your models here.
 
 admin.site.register(MedicineRecord)
 admin.site.register(MedicinePrescription)
+admin.site.register(DoctorNotes)
+admin.site.register(TestResult)
