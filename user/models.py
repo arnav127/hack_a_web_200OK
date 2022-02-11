51cb51b49d534946b04daf66f5575cdd2e753ca5
Gyanendra Shukla <srirajshukla@gmail.com>
2022-02-11 17:38:18 +0530

    refactor: remove unrequired fields from user model

--- user/models.py
+++ user/models.py
@@ -4,13 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 class ExtendUser(AbstractUser):
     email = models.EmailField(blank=False, max_length=255, verbose_name="email")
-    full_name = models.CharField(max_length=255, blank=False, verbose_name="full name")
-    display_name = models.CharField(max_length=255, blank=True, verbose_name="display name")
-    bio = models.TextField(blank=True, verbose_name="bio")
-    location = models.CharField(max_length=255, blank=True, verbose_name="location")
-
-    is_doctor = models.BooleanField(default=False)
-    is_patient = models.BooleanField(default=False)
 
     USERNAME_FIELD = "username"
     EMAIL_FIELD = "email"
