b23d0cb45e944e92ed5227b89bcea206bfd94c9b
Gyanendra Shukla <srirajshukla@gmail.com>
2022-02-11 23:26:28 +0530

    feat: add update hospital resource mutation

--- resources/schema.py
+++ resources/schema.py
@@ -63,6 +63,43 @@ class CreateHospitalResource(graphene.Mutation):
         hospital_resource, _ = HospitalResource.objects.get_or_create(hospital=info.context.user.hospital, defaults={**kwargs})
         return CreateHospitalResource(ok=True, hospital_resource=hospital_resource)
 
+class UpdateHospitalResource(graphene.Mutation):
+    ok = graphene.Boolean()
+    hospital_resource = graphene.Field(HospitalResourceType)
+
+    class Arguments:
+        id = graphene.Int()
+        bed_capacity = graphene.Int()
+        bed_available = graphene.Int()
+        ventilator_capacity = graphene.Int()
+        ventilator_available = graphene.Int()
+        icu_capacity = graphene.Int()
+        icu_available = graphene.Int()
+
+        blood_test = graphene.Boolean()
+        urine_test = graphene.Boolean()
+
+        xray = graphene.Boolean()
+        ultrasound = graphene.Boolean()
+        mri = graphene.Boolean()
+        ekg = graphene.Boolean()
+        ecg = graphene.Boolean()
+        eeg = graphene.Boolean()
+        catscan = graphene.Boolean()
+
+        mammogram = graphene.Boolean()
+        colonoscopy = graphene.Boolean()
+
+    @classmethod
+    @login_required
+    def mutate(cls, root, info, **kwargs):
+        hospital_resource = HospitalResource.objects.get(hospital=info.context.user.hospital)
+        for key, value in kwargs.items():
+            setattr(hospital_resource, key, value)
+
+        hospital_resource.save()
+        return UpdateHospitalResource(ok=True, hospital_resource=hospital_resource)
 
 class HospitalResourceMutation(graphene.ObjectType):
     create_hospital_resource = CreateHospitalResource.Field()
+    update_hospital_resource = UpdateHospitalResource.Field()
