3d3631aebb6c1a0d5c150390abebf3c02204979c
Gyanendra Shukla <srirajshukla@gmail.com>
2022-02-11 20:34:35 +0530

    feat: Add MedicineRecord

--- patientrecord/admin.py
+++ patientrecord/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from patientrecord.models import MedicinePrescription, MedicineRecord
+
 # Register your models here.
+
+admin.site.register(MedicineRecord)
+admin.site.register(MedicinePrescription)
--- patientrecord/models.py
+++ patientrecord/models.py
@@ -1,3 +1,21 @@
+from typing import Any
 from django.db import models
+from user.models import Hospital, Patient
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+
+class MedicinePrescription(models.Model):
+    medicine = models.CharField(max_length=255, blank=False, verbose_name="Medicine Name")
+    doses = models.CharField(max_length=255, blank=True, verbose_name="Doses")
+
+    def __str__(self) -> str:
+        return f'{self.medicine} - {self.doses}'
+
+class MedicineRecord(models.Model):
+    patient = models.OneToOneField(Patient, on_delete=models.CASCADE)
+    prescriptions = models.ManyToManyField(MedicinePrescription, blank=False)
+
+    def __str__(self) -> str:
+        return f'MedRec - {self.patient.name}'
+
