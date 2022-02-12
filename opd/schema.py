f4ab6ef7d3d99895b279989ad5ba3489af2bea41
Gyanendra Shukla <srirajshukla@gmail.com>
2022-02-12 10:41:28 +0530

    feat: update current patient status

--- opd/schema.py
+++ opd/schema.py
@@ -92,3 +92,34 @@ class CreateDoctorPatientAssigned(graphene.Mutation):
 
         return CreateDoctorPatientAssigned(ok=True, doctor_patient_assigned=doctor_patient_assigned)
 
+class ChangeDoctorPatientAssignedStatus(graphene.Mutation):
+    ok = graphene.Boolean()
+    doctor_patient_assigned = graphene.Field(DoctorPatientAssignedType)
+
+    class Arguments:
+        patient_id = graphene.String(required=True)
+        doctor_id = graphene.String(required=True)
+        new_status = graphene.String(required=True)
+        
+
+    @classmethod
+    @login_required
+    def mutate(cls, root, info, patient_id, doctor_id, new_status):
+        if not info.context.user.is_hospital:
+            return ChangeDoctorPatientAssignedStatus(ok=False, doctor_patient_assigned=None)
+
+        # check if the hospital is authorized to assign the patient to the doctor
+        authorized_hospital_list = PatientAuthorizedHospital.objects.filter(patient_id=patient_id, hospital_id=info.context.user.hospital).first()
+        if not authorized_hospital_list:
+            return ChangeDoctorPatientAssignedStatus(ok=False, doctor_patient_assigned=None)
+
+        doctor_patient_assigned = DoctorPatientAssigned.objects.filter(doctor_id=doctor_id, patient_id=patient_id).last()
+        if not doctor_patient_assigned:
+            return ChangeDoctorPatientAssignedStatus(ok=False, doctor_patient_assigned=None)
+
+        # change the status to new status
+        doctor_patient_assigned.status = new_status
+        doctor_patient_assigned.save()
+
+        return ChangeDoctorPatientAssignedStatus(ok=True, doctor_patient_assigned=doctor_patient_assigned)
+
