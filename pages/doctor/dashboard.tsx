e784438012206391e656c54adeeecea4fd662352
Seshal Jain <seshaljain11@gmail.com>
2022-02-12 22:33:01 +0530

    feat(doctor): add dashboard

---
+++ pages/doctor/dashboard.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+import Layout from '../../components/Hospital/Layout'
+import { useAuth } from '../../lib/auth'
+
+const Dashboard = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const router = useRouter()
+
+  useEffect(() => {
+    const loadUser = () => {
+      const isAuthenticated = localStorage.getItem('user')
+      if (!isAuthenticated) router.replace('/hospital/login')
+    }
+    loadUser()
+    setIsLoading(false)
+  }, [])
+
+  return isLoading ? (
+    'Loading...'
+  ) : (
+    <Layout title="Dashboard">Doctor dashboard</Layout>
+  )
+}
+
+export default Dashboard
