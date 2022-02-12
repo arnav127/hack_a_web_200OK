fd2c26c7f6849d7d885125bdb676d4452af9edb1
Seshal Jain <seshaljain11@gmail.com>
2022-02-12 18:50:31 +0530

    feat(hospital/patients): update view page

--- pages/hospital/patients/view.tsx
+++ pages/hospital/patients/view.tsx
@@ -32,10 +32,10 @@ const Dashboard = () => {
                                 Diagnosis
                             </th>
                             <th className={styles.colHead}>
-                                Discharge
+                                Admitted On
                             </th>
                             <th className={styles.colHead}>
-                                Refer
+                                Status
                             </th>
                         </tr>
                     </thead>
@@ -43,20 +43,32 @@ const Dashboard = () => {
                     <tbody>
                         {
                             data?.me?.hospital?.patientauthorizedhospitalSet.map(patient =>
-                                <tr key={patient.patientId.id}>
-                                    <th className={styles.rowHead}>
-                                        {patient.patientId.name}
-                                    </th>
-                                    <td className={styles.td}>
-                                        {patient.patientId.phone}
-                                    </td>
-                                    <td className={styles.td}>
-                                        {patient.patientId.doctorPatientAssignedSet &&
-                                            patient.patientId.doctorPatientAssignedSet.map(doctor => doctor.doctor.name)}
-                                    </td>
-                                    <td className={styles.td}>
-                                    </td>
-                                </tr>
+                                <Link href={`/hospital/patients/${patient.patientId.id}`}>
+                                    <tr key={patient.patientId.id}>
+                                        <th className={styles.rowHead}>
+                                            {patient.patientId.name}
+                                        </th>
+                                        <td className={styles.td}>
+                                            {patient.patientId.phone}
+                                        </td>
+                                        <td className={styles.td}>
+                                            {patient?.patientId?.doctorpatientassignedSet.length > 0 ?
+                                                patient?.patientId?.doctorpatientassignedSet.at(-1).doctor.name : "-"}
+                                        </td>
+                                        <td className={styles.td}>
+                                            {patient?.patientId.doctornotesSet.length > 0 ?
+                                                patient?.patientId?.doctornotesSet.map(diagnosis => diagnosis.diagnosis) : "-"}
+                                        </td>
+                                        <td className={styles.td}>
+                                            {patient?.patientId?.doctorpatientassignedSet.length > 0 ?
+                                                patient?.patientId?.doctorpatientassignedSet.at(-1).assignedAt.slice(0, 10) : "-"}
+                                        </td>
+                                        <td className={styles.td}>
+                                            {patient?.patientId?.doctorpatientassignedSet.length > 0 ?
+                                                patient?.patientId?.doctorpatientassignedSet.at(-1).status : "-"}
+                                        </td>
+                                    </tr>
+                                </Link>
                             )
                         }
                     </tbody>
