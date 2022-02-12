1aaa2b060d39a1d9dddc19a4ed0533c5877d6209
Gyanendra Shukla <srirajshukla@gmail.com>
2022-02-12 09:10:23 +0530

    feat: add doctor and patient assigned models

--- opd/admin.py
+++ opd/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from opd.models import Doctor, DoctorPatientAssigned
+
 # Register your models here.
+
+admin.site.register(Doctor)
+admin.site.register(DoctorPatientAssigned)
--- opd/models.py
+++ opd/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from user.models import ExtendUser, Hospital, Patient
 # Create your models here.
+
+class Doctor(models.Model):
+    user = models.OneToOneField(ExtendUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=False, verbose_name="Doctor Name")
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+
+    specialization = models.CharField(max_length=255, blank=False, verbose_name="Specialization")
+
+    def __str__(self):
+        return f"{self.name} - {self.hospital.name}"
+
+class DoctorPatientAssigned(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    assigned_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=255, blank=False, verbose_name="Status")
+
+    def __str__(self) -> str:
+        return f'{self.patient.name} - {self.doctor.name} - {self.status}'
