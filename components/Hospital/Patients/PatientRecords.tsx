ed660c710b5d6bf42a5a87cb29f230e7233ba048
Seshal Jain <seshaljain11@gmail.com>
2022-02-12 21:46:57 +0530

    feat(patientRecords): CHAL GAYA BHAI

--- components/Hospital/Patients/PatientRecords.tsx
+++ components/Hospital/Patients/PatientRecords.tsx
@@ -1,32 +1,103 @@
-import { useState } from 'react';
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { storage } from '../../../lib/fileUpload';
 
 import {
+    useCreateTestResultMutation
 } from '../../../graphql/generated'
 
-import { uploadFile } from '../../../lib/fileUpload';
 
 const PatientRecords = ({ pid, data }) => {
-    const [file, setFile] = useState(null)
+    const [createTestResultMutation] = useCreateTestResultMutation();
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        const formData = new FormData(e.currentTarget)
+        const file = formData.get('media');
 
+        if (file.name) {
+            const date = new Date();
+            const fsFileName = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${date.getTime()}-${file.name}`;
+            const storageRef = ref(storage, fsFileName);
 
-        let downloadLoc = "";
-        if (file == null) return;
+            const uploadTask = uploadBytesResumable(storageRef, file, {});
 
-        uploadFile(file, file?.name)
-            .then(res => {
-                downloadLoc = res
+            uploadTask.on('state_changed',
+                (snapshot) => {
+                    switch (snapshot.state) {
+                        case 'paused':
+                            console.log('Upload is paused');
+                            break;
+                        case 'running':
+                            break;
+                    }
+                },
+                (error) => {
+                    switch (error.code) {
+                        case 'storage/unauthorized':
+                            break;
+                        case 'storage/canceled':
+                            break;
+                        case 'storage/unknown':
+                            break;
+                    }
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((res) => {
+                        console.log(res)
+                        createTestResultMutation({
+                            variables: {
+                                media: res,
+                                patientId: pid,
+                                testName: formData.get('testName'),
+                                testResult: formData.get('testResult'),
+                            }
+                        })
+                    })
+                });
+        }
+        else {
+            createTestResultMutation({
+                variables: {
+                    patientId: pid,
+                    testName: formData.get('testName'),
+                    testResult: formData.get('testResult'),
+                }
             })
-            .catch(err => {
-                console.log(err);
-            })
+
+        }
+
     }
     return (
         <div>
             <h2>Patient Records</h2>
-            <input type="file" onChange={(e) => { setFile(e.target.files[0]) }} />
-            <button onClick={handleSubmit}>Upload</button>
+            <form
+                className="flex w-96 max-w-[24rem] flex-col gap-4 rounded-lg bg-sky-200/50 p-8"
+                onSubmit={handleSubmit}
+            >
+                <input
+                    type="text"
+                    id="testName"
+                    name="testName"
+                    className="rounded-lg border border-transparent py-2 px-4 text-gray-700 placeholder-gray-400 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-sky-600"
+                    placeholder="Test Name"
+                    required
+                />
+                <input
+                    type="text"
+                    id="testResult"
+                    name="testResult"
+                    className="rounded-lg border border-transparent py-2 px-4 text-gray-700 placeholder-gray-400 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-sky-600"
+                    placeholder="Test Result"
+                    required
+                />
+                <input type="file" name="media" id='media' />
+                <button
+                    type="submit"
+                    className="w-full rounded-lg bg-sky-600 py-2 px-4 text-center font-semibold text-white shadow-md transition duration-200 ease-in hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 focus:ring-offset-sky-200"
+                >
+                    Upload
+                </button>
+            </form>
         </div>
     )
 }
--- lib/fileUpload.ts
+++ lib/fileUpload.ts
@@ -10,64 +10,6 @@ const firebaseConfig = {
     appId: process.env.NEXT_PUBLIC_APPID,
 };
 
-
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
-
-
-export const uploadFile = async (file: Buffer, filename: string, metadata: any): Promise<string> => {
-    const date = new Date();
-    const fsFileName = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${date.getTime()}-${filename}`;
-    const storageRef = ref(storage, fsFileName);
-
-    const uploadTask = uploadBytesResumable(storageRef, file, metadata);
-
-    let fileDownloadURL = "";
-
-    // Listen for state changes, errors, and completion of the upload.
-    uploadTask.on('state_changed',
-        (snapshot) => {
-            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            // console.log('Upload is ' + progress + '% done');
-            switch (snapshot.state) {
-                case 'paused':
-                    console.log('Upload is paused');
-                    break;
-                case 'running':
-                    // console.log('Upload is running');
-                    break;
-            }
-        },
-        (error) => {
-            // A full list of error codes is available at
-            // https://firebase.google.com/docs/storage/web/handle-errors
-            switch (error.code) {
-                case 'storage/unauthorized':
-                    // User doesn't have permission to access the object
-                    break;
-                case 'storage/canceled':
-                    // User canceled the upload
-                    break;
-
-                // ...
-
-                case 'storage/unknown':
-                    // Unknown error occurred, inspect error.serverResponse
-                    break;
-            }
-        },
-        () => {
-            // Upload completed successfully, now we can get the download URL
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                // console.log('File available at', downloadURL);
-                fileDownloadURL = downloadURL;
-            });
-        });
-
-    return fileDownloadURL;
-}
-
-
+export const storage = getStorage(app);
