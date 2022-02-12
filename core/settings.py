a5650b8e0b6aea6014664977f31d5e232aff21ca
Arnav Dixit <arnavdixit127@gmail.com>
2022-02-12 22:50:06 +0530

    fix: modify static root

--- core/settings.py
+++ core/settings.py
@@ -19,12 +19,8 @@ from os import path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
-STATICFILES_DIRS = (
-    path.join(BASE_DIR, 'static'),
-)
 
 
 env = environ.Env()
