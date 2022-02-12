f4d69b31eef2ac89143cc287a830fd27f197951b
Gyanendra Shukla <srirajshukla@gmail.com>
2022-02-12 10:40:53 +0530

    feat: add create doctor patient assigned mutation

--- opd/schema.py
+++ opd/schema.py
@@ -3,7 +3,7 @@ import graphene
 from graphene_django import DjangoObjectType
 from graphql_jwt.decorators import login_required
 from opd.models import Doctor, DoctorPatientAssigned
-from user.models import ExtendUser
+from user.models import ExtendUser, Patient, PatientAuthorizedHospital
 
 
 class DoctorType(DjangoObjectType):
@@ -60,3 +60,35 @@ class DoctorPatientAssignedQuery(graphene.ObjectType):
         if info.context.user.is_doctor:
             return DoctorPatientAssigned.objects.filter(doctor=info.context.user.doctor)
         return None
+
+
+class CreateDoctorPatientAssigned(graphene.Mutation):
+    doctor_patient_assigned = graphene.Field(DoctorPatientAssignedType)
+    ok = graphene.Boolean()
+
+    class Arguments:
+        doctor_id = graphene.String(required=True)
+        patient_id = graphene.String(required=True)
+        status = graphene.String()
+
+    @classmethod
+    @login_required
+    def mutate(cls, root, info, doctor_id, patient_id, **kwargs):
+        if not info.context.user.is_hospital:
+            return CreateDoctorPatientAssigned(ok=False, doctor_patent_assigned=None)
+
+        # check if the hospital is authorized to assign the patient to the doctor
+        authorized_hospital_list = PatientAuthorizedHospital.objects.filter(patient_id=patient_id, hospital_id=info.context.user.hospital).first()
+        if not authorized_hospital_list:
+            return CreateDoctorPatientAssigned(ok=False, doctor_patient_assigned=None)
+
+        doctor = Doctor.objects.get(pk=doctor_id)
+        patient = Patient.objects.get(pk=patient_id)
+        doctor_patient_assigned = DoctorPatientAssigned(doctor=doctor, patient=patient)
+
+        for (key, value) in kwargs.items():
+            setattr(doctor_patient_assigned, key, value)
+        doctor_patient_assigned.save()
+
+        return CreateDoctorPatientAssigned(ok=True, doctor_patient_assigned=doctor_patient_assigned)
+
