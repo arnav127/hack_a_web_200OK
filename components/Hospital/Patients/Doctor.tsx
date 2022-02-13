a8bfbb2256b607244e6d35ffe03d57eb5c8952d3
Seshal Jain <seshaljain11@gmail.com>
2022-02-13 11:02:26 +0530

    fix(patient/record): allow hospital to change status

--- components/Hospital/Patients/Doctor.tsx
+++ components/Hospital/Patients/Doctor.tsx
@@ -68,28 +68,26 @@ const Doctor = ({ pid, data }) => {
                         </p>
                         <p>
                             Status: {data?.patient?.doctorpatientassignedSet.at(-1).status}
-                            {user?.isDoctor && (
-                                <form
-                                    className="flex gap-4"
-                                    onSubmit={handleDoctorStatusUpdate}
+                            <form
+                                className="flex gap-4"
+                                onSubmit={handleDoctorStatusUpdate}
+                            >
+                                Update status?
+                                <select name="newStatus" id="newStatus" required>
+                                    <option value="">Select option</option>
+                                    <option value="CURED">CURED</option>
+                                    <option value="ASSIGNED">ASSIGNED</option>
+                                    <option value="REFERRED">REFERRED</option>
+                                    <option value="WAITING">WAITING</option>
+                                </select>
+                                <button
+                                    type="submit"
+                                    className="w-full rounded-lg bg-sky-600 py-2 px-4 text-center font-semibold text-white shadow-md transition duration-200 ease-in hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 focus:ring-offset-sky-200"
                                 >
-                                    Update status?
-                                    <select name="newStatus" id="newStatus" required>
-                                        <option value="">Select option</option>
-                                        <option value="CURED">CURED</option>
-                                        <option value="ASSIGNED">ASSIGNED</option>
-                                        <option value="REFERRED">REFERRED</option>
-                                        <option value="WAITING">WAITING</option>
-                                    </select>
-                                    <button
-                                        type="submit"
-                                        className="w-full rounded-lg bg-sky-600 py-2 px-4 text-center font-semibold text-white shadow-md transition duration-200 ease-in hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 focus:ring-offset-sky-200"
-                                    >
-                                        Submit
-                                    </button>
-                                </form>
+                                    Submit
+                                </button>
+                            </form>
 
-                            )}
                         </p>
 
                         <div>
