fb43b602f26e6f5a201f398d126174cd8033dcb1
Arnav Dixit <arnavdixit127@gmail.com>
2022-02-13 09:14:07 +0530

    fix: excluded completed patients

--- opd/schemas/doctor_patient_assigned.py
+++ opd/schemas/doctor_patient_assigned.py
@@ -20,7 +20,7 @@ class DoctorPatientAssignedQuery(graphene.ObjectType):
 
     def resolve_doctor_patient_assigned(root, info):
         if info.context.user.is_doctor:
-            return DoctorPatientAssigned.objects.filter(doctor=info.context.user.doctor)
+            return DoctorPatientAssigned.objects.filter(doctor=info.context.user.doctor).exclude(status__in=["DONE", "REFERRED"])
         return None
 
 
