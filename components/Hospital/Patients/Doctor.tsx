65faad38be1ee550b0c8b185294fc0e342be4d2c
Seshal Jain <seshaljain11@gmail.com>
2022-02-13 10:25:34 +0530

    feat(patient/record): add dropdown to patient status

--- components/Hospital/Patients/Doctor.tsx
+++ components/Hospital/Patients/Doctor.tsx
@@ -59,14 +59,13 @@ const Doctor = ({ pid, data }) => {
                                     onSubmit={handleDoctorStatusUpdate}
                                 >
                                     Update status?
-                                    <input
-                                        type="text"
-                                        id="newStatus"
-                                        name="newStatus"
-                                        className="rounded-lg border border-transparent py-2 px-4 text-gray-700 placeholder-gray-400 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-sky-600"
-                                        placeholder="New status"
-                                        required
-                                    />
+                                    <select name="newStatus" id="newStatus" required>
+                                        <option value="">Select option</option>
+                                        <option value="CURED">CURED</option>
+                                        <option value="ASSIGNED">ASSIGNED</option>
+                                        <option value="REFERRED">REFERRED</option>
+                                        <option value="WAITING">WAITING</option>
+                                    </select>
                                     <button
                                         type="submit"
                                         className="w-full rounded-lg bg-sky-600 py-2 px-4 text-center font-semibold text-white shadow-md transition duration-200 ease-in hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 focus:ring-offset-sky-200"
