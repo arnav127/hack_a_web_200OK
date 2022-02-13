2b007f5173e2faa2a4ebed48b9eadc880066dc5b
Seshal Jain <seshaljain11@gmail.com>
2022-02-13 17:04:45 +0530

    Disease

--- pages/doctor/disease.tsx
+++ pages/doctor/disease.tsx
@@ -2,15 +2,15 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link'
 import Layout from '../../components/Hospital/Layout'
 
-import { usePatientByAadharQuery } from '../../graphql/generated'
+import { useDiseaseInfoQuery } from '../../graphql/generated'
 
 const Admit = () => {
     const [aadhar, setAadhar] = useState(null);
     const [success, setSuccess] = useState(null);
 
-    const { data, loading, error } = usePatientByAadharQuery({
+    const { data, loading, error } = useDiseaseInfoQuery({
         variables: {
-            aadhar: aadhar
+            name: aadhar
         },
         skip: (!aadhar),
     });
@@ -32,42 +32,20 @@ const Admit = () => {
                         type="text"
                         id="aadhar"
                         name="aadhar"
-                        className="py-2 px-4 placeholder-gray-400 text-gray-700 rounded-lg border shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-gray-600"
+                        className="px-4 py-2 placeholder-gray-400 text-gray-700 rounded-lg border shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-gray-600"
                         placeholder="Disease"
                         required
                     />
                     <button
                         type="submit"
-                        className="py-2 px-4 w-36 font-semibold text-center text-white bg-gray-600 rounded-lg shadow-md transition duration-200 ease-in hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 focus:ring-offset-sky-200"
+                        className="px-4 py-2 w-36 font-semibold text-center text-white bg-gray-600 rounded-lg shadow-md transition duration-200 ease-in hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 focus:ring-offset-sky-200"
                     >
                         Submit
                     </button>
                 </form>
 
-                {aadhar && (
-                    data?.patient ? (
-                        <>
-                            <h2>Record found!</h2>
-                            <div className="my-4 rounded-lg bg-gray-100/50 p-8">
-                                <h3>{data.patient.name}</h3>
-                                <p>{data.patient.aadhar}</p>
-                                <p>{data.patient.phone}</p>
-                            </div>
-
-                            {data?.patient?.patientauthorizedhospitalSet.length > 0 ? (
-                                <>
-                                    {console.log(user)}
-                                    {data?.patient?.patientauthorizedhospitalSet.at(-1).hospitalId.id === user?.hospital?.id ?
-                                        (
-                                            <Link
-                                                href={`/hospital/patients/${data?.patient?.id}`}
-                                            >
-                                                <a className="mt-4 py-2 px-4 w-48 font-semibold text-center text-white bg-gray-600 rounded-lg shadow-md transition duration-200 ease-in hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 focus:ring-offset-sky-200" >
-                                                    View details
-                                                </a>
-                                            </Link>
-                                        )})}</>))}
-                        </div>
+{data && <div>{JSON.stringify(data?.diseaseInfo)}</div>}
+            </div>
         </Layout >
     )
 };
