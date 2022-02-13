b13fd634f1711d1ceba8ba0e4634fdc3a4ecb1e3
Seshal Jain <seshaljain11@gmail.com>
2022-02-13 09:45:26 +0530

    feat(doctor): add update doctor status option

--- components/Hospital/Patients/Doctor.tsx
+++ components/Hospital/Patients/Doctor.tsx
@@ -1,14 +1,32 @@
 import {
     useAllDoctorsQuery,
-    useCreateDoctorPatientAssignedMutation
+    PatientByIdDocument,
+    useCreateDoctorPatientAssignedMutation,
+    useUpdateDoctorPatientAssignedMutation
 } from '../../../graphql/generated'
 
+import { useAuth } from '../../../lib/auth'
+
 const Doctor = ({ pid, data }) => {
+    const { user } = useAuth();
     const [createDoctorPatientAssigned] = useCreateDoctorPatientAssignedMutation();
+    const [updateDoctorPatientAssigned] = useUpdateDoctorPatientAssignedMutation();
 
     const { data: doctors } = useAllDoctorsQuery();
 
-    const handleDoctorSubmit = (e) => {
+    const handleDoctorStatusUpdate = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget)
+        updateDoctorPatientAssigned({
+            variables: {
+                id: data?.patient?.doctorpatientassignedSet.at(-1).id,
+                newStatus: formData.get('newStatus')
+            },
+            refetchQueries: [PatientByIdDocument]
+        })
+    }
+
+    const handleAssignDoctor = (e) => {
         e.preventDefault();
         const formData = new FormData(e.currentTarget)
         createDoctorPatientAssigned({
@@ -16,7 +34,8 @@ const Doctor = ({ pid, data }) => {
                 doctorId: formData.get('doctor'),
                 patientId: pid,
                 status: 'ASSIGNED'
-            }
+            },
+            refetchQueries: [PatientByIdDocument]
         })
     }
 
@@ -34,6 +53,29 @@ const Doctor = ({ pid, data }) => {
                         </p>
                         <p>
                             Status: {data?.patient?.doctorpatientassignedSet.at(-1).status}
+                            {user?.isDoctor && (
+                                <form
+                                    className="flex gap-4"
+                                    onSubmit={handleDoctorStatusUpdate}
+                                >
+                                    Update status?
+                                    <input
+                                        type="text"
+                                        id="newStatus"
+                                        name="newStatus"
+                                        className="rounded-lg border border-transparent py-2 px-4 text-gray-700 placeholder-gray-400 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-sky-600"
+                                        placeholder="New status"
+                                        required
+                                    />
+                                    <button
+                                        type="submit"
+                                        className="w-full rounded-lg bg-sky-600 py-2 px-4 text-center font-semibold text-white shadow-md transition duration-200 ease-in hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 focus:ring-offset-sky-200"
+                                    >
+                                        Submit
+                                    </button>
+                                </form>
+
+                            )}
                         </p>
 
                         <div>
@@ -52,7 +94,7 @@ const Doctor = ({ pid, data }) => {
                 ) : (
                     <form
                         className="flex w-96 max-w-[24rem] flex-col gap-4 rounded-lg bg-sky-200/50 p-8"
-                        onSubmit={handleDoctorSubmit}
+                        onSubmit={handleAssignDoctor}
                     >
                         <select name="doctor" id="doctor">
                             <option value="">Select doctor</option>
